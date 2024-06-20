cc4e74e213d33f33bbb62165edc4ed5709197511
Henrycall21 <160125220+Henrycall21@users.noreply.github.com>
2024-06-20 07:56:57 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-const botoes = document.querySelectorAll(".botao")
+const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo")
 const tempo = document.querySelectorAll(".contador")
-tempo[0].textContent="Ola"
-console.log(tempo)
-for (let i = 0; 1 < botoes.length ; i++){
-    botoes[i].onclick = function(){
-        for (let j = 0; j < botoes.length ;j++){
+tempo[0].textContent = "ola"
+const tempoObjetivo1 = new Date(2007,4,16,17);
+const tempoObjetivo2 = new Date(05);
+const tempoObjetivo3 = new Date(2007);
+const tempoObjetivo4 = new Date(17);
+const tempoObjetivo5 = new Date(06);
+const agora = new Date();
+let segundos;
+let minutos;
+let horas;
+let dias;
+Segundos = (tempoObjetivo1 - agora)/1000
+Minutos = segundos/60
+Horas = minutos/60
+Dias = horas/24
+tempo[0].textContent = tempoObjetivo1 - agora
+tempo[1].textContent = tempoObjetivo1
+console.log(botoes);
+for(let i = 0; i < botoes.length ; i++){
+    botoes[i].onclick = function() {
+        for (j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
-            conteudo[1].classList.remove("ativo")
+            conteudo[j].classList.remove("ativo")
         }
         botoes[i].classList.add("ativo")
-        conteudo[1].classList.add("ativo")
+        conteudo[i].classList.add("ativo")
     }
 }
